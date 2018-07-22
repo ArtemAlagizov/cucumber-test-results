5922c14e18fc866061df947ec59a1438dc19406b
ArtemAlagizov <alagizov@gmail.com>
2018-07-22 15:18:21 +0200

    added basic header styling for features page

--- src/components/HeaderFeatures/HeaderFeatures.js
+++ src/components/HeaderFeatures/HeaderFeatures.js
@@ -1,6 +1,10 @@
 import React, {Component} from 'react';
+import CircularProgressbar from 'react-circular-progressbar';
+import 'react-circular-progressbar/dist/styles.css';
+
 import './HeaderFeatures.css';
 import TimeUtils from '../TimeUtils/TimeUtils';
+import cucumberLogo from '../../rickle.png';
 
 class HeaderFeatures extends Component {
     render() {
@@ -8,11 +12,31 @@ class HeaderFeatures extends Component {
         const overallDuration = TimeUtils.convertNanosecondsToTime(headerData.overallDuration);
         const featuresNumber = headerData.featuresNumber;
         const numberOfPassedFeatures = headerData.numberOfPassedFeatures;
+        const percentageOfPassed = numberOfPassedFeatures / featuresNumber * 100;
+        const circularIndicatorText = `${numberOfPassedFeatures}/${featuresNumber}`;
 
         return (
             <div className="header-features">
-                <div>passed: {numberOfPassedFeatures}/{featuresNumber}</div>
-                <div>duration: {overallDuration}</div>
+                <div className="header-title">
+                    <div className="header-title-container">
+                        <div>Cucumber</div>
+                        <div>LANA</div>
+                    </div>
+                    <div className="cucumber-logo-container">
+                        <img src={cucumberLogo} className="cucumber-logo"/>
+                    </div>
+                </div>
+                <div className="header-statistics">
+                    <div className="header-statistics-container">
+                        <div className="test-status">
+                            <CircularProgressbar percentage={percentageOfPassed} text={circularIndicatorText}/>
+                        </div>
+                        <div className="duration-container">
+                            <div>Overall time</div>
+                            <div>{overallDuration}</div>
+                        </div>
+                    </div>
+                </div>
             </div>
         )
     }
