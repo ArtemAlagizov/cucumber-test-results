d3debd3adf1706cb1523b311a8087b041dcac15e
ArtemAlagizov <alagizov@gmail.com>
2018-08-12 18:57:15 +0200

    minor style change

--- src/validation/validator.js
+++ src/validation/validator.js
@@ -4,7 +4,7 @@ import Schema from './schema';
 
 class Validator {
     static getValidationFunction() {
-        const ajv = new Ajv({ allErrors: true });
+        const ajv = new Ajv({allErrors: true});
 
         return ajv.compile(Schema);
     }
