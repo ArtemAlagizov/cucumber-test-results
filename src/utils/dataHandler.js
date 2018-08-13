9599eec98f73214dbe1771e7fe6ce18f991d8bcf
ArtemAlagizov <alagizov@gmail.com>
2018-08-13 20:23:41 +0200

    added more tests, restructured dataHandler

--- src/utils/dataHandler.js
+++ src/utils/dataHandler.js
@@ -19,60 +19,114 @@ class DataHandler {
         };
     }
 
-    static addRelevantInformation(features) {
+    static setDurationPerFeature() {
+        const getFeatureDuration = elements => elements.reduce((prev, element) => prev + this.sumDuration()(element.steps), 0);
+        const setFeatureDuration = feature => feature.duration = getFeatureDuration(feature.elements);
+
+        return features => features.map(feature => setFeatureDuration(feature));
+    }
+
+    static setResultPerFeature() {
+        const getFeatureResult = elements => elements.reduce((prev, element) => prev && this.getTestsPassedPerScenario()(element.steps), true);
+        const setFeatureResult = feature => feature.testsPassed = getFeatureResult(feature.elements);
+
+        return features => features.map(feature => setFeatureResult(feature));
+    }
+
+    static getTestsPassed() {
+        return step => step.result ? step.result.status ? step.result.status === 'passed' : true : true;
+    }
+
+    static getTestsPassedPerScenario() {
+        const andTestsPassedIfExists = (prev, step) => this.getTestsPassed()(step) && prev;
+
+        return steps => steps.reduce((prev, step) => andTestsPassedIfExists(prev, step), true);
+    }
+
+    static sumDuration() {
         const getDuration = step => step.result ? step.result.duration ? step.result.duration : 0 : 0;
         const addDurationIfExists = (prev, step) => getDuration(step) + prev;
-        const sumDuration = steps => steps.reduce((prev, step) => addDurationIfExists(prev, step), 0);
-        const getTestsPassed = step => step.result ? step.result.status ? step.result.status === 'passed' : true : true;
-        const getStatus = step => step.result ? step.result.status ? step.result.status : 'failed' : 'failed';
-        const andTestsPassedIfExists = (prev, step) => getTestsPassed(step) && prev;
-        const getTestsPassedPerScenario = steps => steps.reduce((prev, step) => andTestsPassedIfExists(prev, step), true);
-        const getFeatureDuration = elements => elements.reduce((prev, element) => prev + sumDuration(element.steps), 0);
-        const getFeatureResult = elements => elements.reduce((prev, element) => prev && getTestsPassedPerScenario(element.steps), true);
-        const setScenarioDuration = element => element.duration = sumDuration(element.steps);
-        const setDurationPerStep = element => element.steps.map(step => step.duration = step.result.duration || 0);
-        const setResultPerStep = element => element.steps.map(step => step.testsPassed = getTestsPassed(step));
-        const setStatusPerStep = element => element.steps.map(step => step.status = getStatus(step));
+
+        return steps => steps.reduce((prev, step) => addDurationIfExists(prev, step), 0);
+    }
+
+    static setDurationPerScenarioPerFeature() {
+        const setScenarioDuration = element => element.duration = this.sumDuration()(element.steps);
         const setDurationPerScenario = feature => feature.elements.map(element => setScenarioDuration(element));
-        const setDurationPerStepPerScenario = feature => feature.elements.map(element => setDurationPerStep(element));
-        const setScenarioResult = element => element.testsPassed = getTestsPassedPerScenario(element.steps);
+
+        return features => features.map(feature => setDurationPerScenario(feature));
+    }
+
+    static setResultPerScenarioPerFeature() {
+        const setScenarioResult = element => element.testsPassed = this.getTestsPassedPerScenario()(element.steps);
         const setResultPerScenario = feature => feature.elements.map(element => setScenarioResult(element));
-        const setResultPerStepPerScenario = feature => feature.elements.map(element => setResultPerStep(element));
-        const setStatusPerStepPerScenario = feature => feature.elements.map(element => setStatusPerStep(element));
-        const setFeatureDuration = feature => feature.duration = getFeatureDuration(feature.elements);
-        const setDurationPerFeature = features => features.map(feature => setFeatureDuration(feature));
+
+        return features => features.map(feature => setResultPerScenario(feature));
+    }
+
+    static setScenariosNumberPerFeature() {
         const setFeatureScenariosNumber = feature => feature.scenariosNumber = feature.elements.length;
-        const setScenariosNumberPerFeature = features => features.map(feature => setFeatureScenariosNumber(feature));
-        const setFeatureResult = feature => feature.testsPassed = getFeatureResult(feature.elements);
-        const setResultPerFeature = features => features.map(feature => setFeatureResult(feature));
-        const setDurationPerScenarioPerFeature = features => features.map(feature => setDurationPerScenario(feature));
-        const setDurationPerStepPerScenarioPerFeature = features => features.map(feature => setDurationPerStepPerScenario(feature));
-        const setResultPerScenarioPerFeature = features => features.map(feature => setResultPerScenario(feature));
-        const setResultPerStepPerScenarioPerFeature = features => features.map(feature => setResultPerStepPerScenario(feature));
-        const setStatusPerStepPerScenarioPerFeature = features => features.map(feature => setStatusPerStepPerScenario(feature));
+
+        return features => features.map(feature => setFeatureScenariosNumber(feature));
+    }
+
+    static setDurationPerStepPerScenarioPerFeature() {
+        const setDurationPerStep = element => element.steps.map(step => step.duration = step.result.duration || 0);
+        const setDurationPerStepPerScenario = feature => feature.elements.map(element => setDurationPerStep(element));
+
+        return features => features.map(feature => setDurationPerStepPerScenario(feature));
+    }
+
+    static setResultPerStepPerScenarioPerFeature() {
+        const setResultPerStep = element => element.steps.map(step => step.testsPassed = this.getTestsPassed()(step));
+        const setResultPerStepPerScenario = feature => feature.elements.map(element => setResultPerStep(element));
+
+        return features => features.map(feature => setResultPerStepPerScenario(feature));
+    }
+
+    static setStatusPerStepPerScenarioPerFeature() {
+        const getStatus = step => step.result ? step.result.status ? step.result.status : 'failed' : 'failed';
+        const setStatusPerStep = element => element.steps.map(step => step.status = getStatus(step));
+        const setStatusPerStepPerScenario = feature => feature.elements.map(element => setStatusPerStep(element));
+
+        return features => features.map(feature => setStatusPerStepPerScenario(feature));
+    }
+
+    static setMaxScenarioTimePerFeature() {
         const getMaxScenarioTime = elements => max(...elements.map(element => element.duration));
-        const setMaxScenarioTimePerFeature = features => features.map(feature => feature.maxScenarioTime = getMaxScenarioTime(feature.elements));
+
+        return features => features.map(feature => feature.maxScenarioTime = getMaxScenarioTime(feature.elements));
+    }
+
+    static setTimeRatePerScenarioPerFeature() {
         const setScenarioTimeRate = (element, feature) => element.timeRate = element.duration / feature.maxScenarioTime;
         const setTimeRatePerScenario = feature => feature.elements.map(element => setScenarioTimeRate(element, feature));
-        const setTimeRatePerScenarioPerFeature = features => features.map(feature => setTimeRatePerScenario(feature));
+
+        return features => features.map(feature => setTimeRatePerScenario(feature));
+    }
+
+    static setTimeRatePerStepPerScenarioPerFeature() {
         const setStepTimeRate = (steps, maxTime) => steps.map(step => step.timeRate = step.duration / maxTime);
         const setTimeRatePerStep = (elements, maxTime) => elements.map(element => setStepTimeRate(element.steps, maxTime));
         const setTimeRatePerStepPerScenario = feature => setTimeRatePerStep(feature.elements, feature.maxScenarioTime);
-        const setTimeRatePerStepPerScenarioPerFeature = features => features.map(feature => setTimeRatePerStepPerScenario(feature));
 
+        return features => features.map(feature => setTimeRatePerStepPerScenario(feature));
+    }
+
+    static addRelevantInformation(features) {
         const featuresClone = clone(features);
         const modifications = [
-            setDurationPerFeature,
-            setResultPerFeature,
-            setDurationPerScenarioPerFeature,
-            setResultPerScenarioPerFeature,
-            setScenariosNumberPerFeature,
-            setDurationPerStepPerScenarioPerFeature,
-            setResultPerStepPerScenarioPerFeature,
-            setStatusPerStepPerScenarioPerFeature,
-            setMaxScenarioTimePerFeature,
-            setTimeRatePerScenarioPerFeature,
-            setTimeRatePerStepPerScenarioPerFeature
+            this.setDurationPerFeature(),
+            this.setResultPerFeature(),
+            this.setDurationPerScenarioPerFeature(),
+            this.setResultPerScenarioPerFeature(),
+            this.setScenariosNumberPerFeature(),
+            this.setDurationPerStepPerScenarioPerFeature(),
+            this.setResultPerStepPerScenarioPerFeature(),
+            this.setStatusPerStepPerScenarioPerFeature(),
+            this.setMaxScenarioTimePerFeature(),
+            this.setTimeRatePerScenarioPerFeature(),
+            this.setTimeRatePerStepPerScenarioPerFeature()
         ];
 
         juxt(modifications)(featuresClone);
