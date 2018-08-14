da19415a9da4dba6fdb996bb57c50ca0cda76e30
ArtemAlagizov <alagizov@gmail.com>
2018-08-14 16:54:10 +0200

    added basic rendering tests for features components

--- src/screens/features/components/featureList/featureTile/component.js
+++ src/screens/features/components/featureList/featureTile/component.js
@@ -12,24 +12,31 @@ class FeatureTile extends Component {
     }
 
     render() {
-        const feature = this.props.feature;
-        const featureStatusClass = feature.testsPassed ? 'passed' : 'failed';
+        const feature = this.props.feature || {
+            name: '',
+            scenariosNumber: 0,
+            duration: 0
+        };
+        const featureName = feature.name || '';
+        const featureScenariosNumber = feature.scenariosNumber || 0;
+        const featureDuration = feature.duration || 0;
+        const featureStatusClass = feature ? feature.testsPassed ? 'passed' : 'failed' : '';
 
         return (
             <div className="feature-tile" onClick={this.handleOnClick}>
                 <div className="feature-tile-content">
                     <div className="feature-tile-upper">
-                        <div className="dashboard-name">{feature.name}</div>
+                        <div className="dashboard-name">{featureName}</div>
                     </div>
                     <div className="feature-tile-downer">
                         <div className="number-of-scenarios">
                             <div className="label">Number of scenarios:</div>
-                            <div>{feature.scenariosNumber}</div>
+                            <div>{featureScenariosNumber}</div>
                         </div>
                         <div className="duration">
                             <div className="label">Duration:</div>
                             <div>
-                                {TimeUtils.convertNanosecondsToTime(feature.duration)}
+                                {TimeUtils.convertNanosecondsToTime(featureDuration)}
                             </div>
                         </div>
                     </div>
