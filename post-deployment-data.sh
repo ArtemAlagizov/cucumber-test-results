70f58ae4694eb4824d5a9cf9f474ee1fa75013ec
Artem Alagizov <artem.alagizov@asml.com>
2019-10-23 11:04:07 +0200

    post-deployment-data.sh

--- post-deployment-data.sh
+++ post-deployment-data.sh
@@ -2,6 +2,7 @@
 
 releaseVersion=$bamboo_inject_target_release_TARGET_RELEASE
 dateTimeOfDeployment=$(date +"%d-%m-%YT%H:%M:%S")
-dd-MM-yyyyTHH:mm:ss
-echo "{releaseVersion:\"${$releaseVersion}\",dateTimeOfDeployment:\"${$dateTimeOfDeployment}\"}" > deploymentData.json
-curl -X POST -H "Content-Type: application/json" -d deploymentData.json http://172.19.234.73:3036/cucumber-report
+pwd
+deploymentDataJsonString='{"releaseVersion":'"\"$releaseVersion\""',"dateTimeOfDeployment":'"\"$dateTimeOfDeployment\""'}'
+echo deploymentDataJsonString: $deploymentDataJsonString
+curl -X POST -H "Content-Type: application/json" -d $deploymentDataJsonString http://172.19.234.73:3036/deployment-data/
